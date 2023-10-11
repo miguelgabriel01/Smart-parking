d402b99b81d45bfda92b7defb18a74efe5872c8e
miguelgabriel01 <gabrielogabriel10@gmail.com>
2023-10-10 23:55:12 -0300

    refactor?: ajuste de exibição e validação de vagas

--- smart-parking-api/pub.py
+++ smart-parking-api/pub.py
@@ -1,9 +1,8 @@
 import random
 import time
-
+import json
 from paho.mqtt import client as mqtt_client
-from datetime import date
-from datetime import datetime
+from datetime import date, datetime
 
 numberPub = random.randint(1, 52)
 
@@ -16,17 +15,14 @@ updateTime = currentTime.strftime('%H:%M')
 broker = 'broker.emqx.io'
 port = 1883
 topic = "python/mqtt"
-# generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print(" ")
-            # print("Connected to MQTT Broker!")
+            print("Connected to MQTT Broker!")
         else:
-            print(" ")
-            # print("Failed to connect, return code %d\n", rc)
+            print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
     client.username_pw_set('emqx', 'public')
@@ -34,7 +30,6 @@ def connect_mqtt():
     client.connect(broker, port)
     return client
 
-# Criação do JSON com 32 registros aleatórios
 dadosSobreAsVagas = []
 for i in range(1, 33):
     situacao = random.choice(["ocupada", "livre"])
@@ -55,46 +50,36 @@ def publish(client):
     print('-----------------------------')
 
     while True:
-        numeroVaga = 0
-        time.sleep(1)
+        print(" ")
+        print("Numero total de vagas no estacionamento: " + str(totalParkingSpaces))
+        print(" ")
+        print("Status atual sobre as vagas: ")
+        print(" ")
+        print(json.dumps(dadosSobreAsVagas, indent=4))
+        print("    ")
+        idSelect = int(input("Digite a vaga que deseja alterar (ou 0 para sair): "))
 
-        result = client.publish(topic, f"{dadosSobreAsVagas}")
-        status = result[0]
-        if status == 0:
-            print(" ")
-            print("Numero total de vagas no estacionamento: " + str(totalParkingSpaces))
-            print(" ")
-            print("Status atual sobre as vagas: ")
-            print(" ")
-            print(dadosSobreAsVagas)
+        if idSelect == 0:
+            break
+
+        if idSelect > 0 and idSelect <= len(dadosSobreAsVagas):
             print("    ")
-            print("    ")
-            idSelect = int(input("Digite a vaga que deseja alterar: "))
-
-            if idSelect:
-                print("    ")
-
-                idSelectStatus = 0
-                if idSelect == dadosSobreAsVagas[idSelect - 1]["id"]:
-                    print("Situação atual da vaga: " + dadosSobreAsVagas[idSelect - 1]["situacao"])
-                    print("Deseja alterar o status desta vaga?")
-                    print("1 - Sim / 2 - Não ")
-                    idSelectStatus = int(input())
-                else:
-                    print("Vaga indisponivel no momento 1!!!")
+            idSelectStatus = 0
+            if idSelect == dadosSobreAsVagas[idSelect - 1]["id"]:
+                print("Situação atual da vaga: " + dadosSobreAsVagas[idSelect - 1]["situacao"])
+                print("Deseja alterar o status desta vaga?")
+                print("1 - Sim / 2 - Não ")
+                idSelectStatus = int(input())
 
                 if idSelectStatus == 1:
-                    if idSelect:
-                        if dadosSobreAsVagas[idSelect - 1]["situacao"] == "ocupada":
-                            dadosSobreAsVagas[idSelect - 1]["situacao"] = "livre"
-                        else:
-                            dadosSobreAsVagas[idSelect - 1]["situacao"] = "ocupada"
+                    if dadosSobreAsVagas[idSelect - 1]["situacao"] == "ocupada":
+                        dadosSobreAsVagas[idSelect - 1]["situacao"] = "livre"
                     else:
-                        print("Vaga indisponivel no momento!!!")
-
+                        dadosSobreAsVagas[idSelect - 1]["situacao"] = "ocupada"
+            else:
+                print("Vaga indisponível no momento!")
         else:
-            print(f"Falhou ao enviar mensagem para o topico {topic}")
-        numeroVaga += 1
+            print("Número de vaga inválido. Tente novamente.")
 
 def run():
     client = connect_mqtt()
